cdce3f8f770fbc499ef016a9b7df605f27228ef0
Maureen Perez <maureen8694@gmail.com>
2022-01-08 14:19:24 -0500

    inital commit

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -7,28 +7,34 @@ namespace ChristmasWithTypes
         static void Main(string[] args)
         {
             var xmas = new Christmas();
-
+    
             var xmasDay = Christmas.Day.Thursday;
+        
 
             //TODO set Santa's name to Kris Kringle
             xmas.Santa = null;
+            string santasName = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
-
-            //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
-
-            Console.WriteLine($"This year christmas falls on {xmasDay} \n");
-            Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
-            Console.WriteLine("Here are the presents we would like:");
-
-            foreach (var present in xmas.Presents)
             {
-                Console.WriteLine($"    {present}");
+                xmas.Presents = new string[3];
+                  
+            string presentOne =  "Iphone 13";
+            string presentTwo = "New Laptop";
+            string presentThree = "Earrings";
+
+       
+            
             }
 
-            Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
+            //TODO Set the TreeHeight to 10
+    
+            int treeHeight = 10;
+
+            Console.WriteLine($"This year christmas falls on {xmasDay}");
+            Console.WriteLine($"\nOur tree will be {10} feet high");
+            Console.WriteLine("\nHere are the presents we would like: \nIphone 13 \nNew Laptop \nEarrings");
+            Console.WriteLine($"\n We like to call Santa, {santasName}");
         }
     }
 }
